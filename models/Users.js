a1e822e6ff2e315a6bb9fe174a3f24e66c2804dd
kanetosan <albertokaneto@gmail.com>
2016-11-03 13:09:22 -0200

    Created UserSchema with username attribute and added a test to see if username is required

---
+++ models/Users.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    username: { type: String, required: true }
+});
+
+module.exports = mongoose.model('User', UserSchema);
--- tests/models/Users_test.js
+++ tests/models/Users_test.js
@@ -1,3 +1,14 @@
 var expect = require('chai').expect;
 
 var User = require('../../models/Users');
+
+describe('user', function() {
+  it('should be invalid if username is empty', function(done) {
+    var user = new User();
+
+    user.validate(function(err) {
+      expect(err.errors.username).to.exist;
+      done();
+    });
+  });
+});
