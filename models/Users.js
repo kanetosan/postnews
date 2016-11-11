1c608f01b914c999c68d7960ed7050802b8faa2b
kanetosan <albertokaneto@gmail.com>
2016-11-11 14:10:51 -0200

    Added unique validator in User model

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, lowercase: true }
+    username: { type: String, required: true, lowercase: true, unique: true }
 });
+
+UserSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', UserSchema);
