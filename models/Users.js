6aef7e015a9b836c047176bcef09f5afd8f4da69
kanetosan <albertokaneto@gmail.com>
2018-04-27 13:06:36 -0300

    Created generateJWT function

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
-var crypto = require('crypto');
 var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
   username: { type: String, required: true, lowercase: true, unique: true }
@@ -16,6 +17,20 @@ UserSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 
   return this.hash === hash;
+}
+
+UserSchema.methods.generateJWT = function() {
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  // The second argument of jwt.sign() is the secret used to sign our tokens. We're hard-coding it in this example, but it is strongly recommended that you use an environment variable for referencing the secret and keep it out of your codebase.
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'SECRET');
 }
 
 UserSchema.plugin(uniqueValidator);
